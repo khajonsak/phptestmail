fc748e8f02ba226cedfa8269261fe2e139937c6e
khajonsak <khajonsak.wo61@snru.ac.th>
2021-08-25 19:27:45 +0700

    ass

--- index.php
+++ index.php
@@ -1,25 +1,12 @@
 <?php
+
 $to = 'khajonsak.wo61@snru.ac.th';
 $subject = 'Marriage Proposal';
+$message = 'Hi Jane, will you marry me?'; 
 $from = 'khajonsak.pok@email.com';
  
-// To send HTML mail, the Content-type header must be set
-$headers  = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
- 
-// Create email headers
-$headers .= 'From: '.$user_email."\r\n".
-    'Reply-To: '.$user_email."\r\n" .
-    'X-Mailer: PHP/' . phpversion();
- 
-// Compose a simple HTML email message
-$message = '<html><body>';
-$message .= '<h1 style="color:#f40;">Hi Jane!</h1>';
-$message .= '<p style="color:#080;font-size:18px;">Will you marry me?</p>';
-$message .= '</body></html>';
- 
 // Sending email
-if(mail($to, $subject, $message, $headers)){
+if(mail($to, $subject, $message)){
     echo 'Your mail has been sent successfully.';
 } else{
     echo 'Unable to send email. Please try again.';
